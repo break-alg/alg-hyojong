68bca0b12d48d495ab9508bb7d8b7e27b05d66bd
HJ-log <gywhd408@naver.com>
2021-02-09 17:11:46 +0900

    fix file structure and add stack_queue solution directorhy

---
+++ programmers/stack_queue/stack_queue-1.py
@@ -0,0 +1,9 @@
+def solution(clothes):
+    answer = 0
+    return answer
+
+# input
+clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+
+# output
+print(solution(clothes))
