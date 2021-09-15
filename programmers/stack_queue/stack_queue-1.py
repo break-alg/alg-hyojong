57aed96784b6bb3a4b1db7cd3f36e4443a365596
hjmoons <gywhd408@naver.com>
2021-09-15 16:19:49 +0900

    delete ordinary files

--- programmers/stack_queue/stack_queue-1.py
+++
@@ -1,9 +0,0 @@
-def solution(clothes):
-    answer = 0
-    return answer
-
-# input
-clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
-
-# output
-print(solution(clothes))
