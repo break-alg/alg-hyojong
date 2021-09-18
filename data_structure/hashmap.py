3fde01b0dee157212e339dcaf538562f2475400f
hjmoons <gywhd408@naver.com>
2021-09-18 23:33:16 +0900

    add hashmap

--- data_structure/hashmap.py
+++ data_structure/hashmap.py
@@ -1,14 +1,73 @@
+# 연결 리스트를 사용하여 HashMap 구현
+# 해시 값 충돌의 경우 개별 체이닝으로 방지
+import collections
+
+
 class ListNode:
     def __init__(self, key=None, value=None):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashMap:
     def __init__(self):
+        self.size = 1000
+        self.table = collections.defaultdict(ListNode)
 
     def put(self, key, value):
+        index = key % self.size
+
+        # 인덱스에 노드가 없다면 삽입 후 종료
+        if self.table[index].value is None:
+            self.table[index] = ListNode(key, value)
+            return
+
+        # 인덱스에 노드가 존재하는 경우 연결 리스트 처리
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.value = value
+                return
+            if p.next is None:
+                break
+            p = p.next
+        p.next = ListNode(key, value)
 
     def get(self, key):
+        index = key % self.size
+        if self.table[index].value is None:
+            return -1
+
+        # 노드가 존재할 떄 일치하는 키 탐색
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.value
+            p = p.next
+        return -1
 
     def remove(self, key):
+        index = key % self.size
+        if self.table[index].value is None:
+            return
+
+        # 인덱스의 첫 번째 노드일 때 삭제 처리
+        p = self.table[index]
+        if p.key == key:
+            self.table[index] = ListNode() if p.next is None else p.next
+            return
+
+        # 연결 리스트 노드 삭제
+        prev = p
+        while p:
+            if p.key == key:
+                prev.next = p.next
+                return
+            prev, p = p, p.next
+
+
+if __name__ == "__main__":
+    hm = HashMap()
+    hm.put(1, 1)
+    hm.put(1, 1)
