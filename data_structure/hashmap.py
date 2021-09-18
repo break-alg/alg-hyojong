51c661803e5067cf92b5a55272de35654c5afdbe
hjmoons <gywhd408@naver.com>
2021-09-18 16:02:04 +0900

    add hashmap class

---
+++ data_structure/hashmap.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.next = None
+
+class HashMap:
+    def __init__(self):
+
+    def put(self, key, value):
+
+    def get(self, key):
+
+    def remove(self, key):
