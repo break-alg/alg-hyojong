e74422cc5cccbc050781729e6e947906071f7dd5
hjmoons <gywhd408@naver.com>
2021-09-23 23:52:26 +0900

    add kakao's problem #1

---
+++ kakao/problem-01.py
@@ -0,0 +1,17 @@
+# 비밀 지도 (★)
+# 2개의 비밀지도를 합치는 문제
+# 비밀지도를
+
+def solution(n, arr1, arr2):
+    map = []
+    for i in range(n):
+        map.append(bin(arr1[i] | arr2[i])[2:].zfill(n).replace('0', " ").replace('1', "#"))
+    return map
+
+
+if __name__ == '__main__':
+    n = 5
+    arr1 = [9, 20, 28, 18, 11]
+    arr2 = [30, 1, 21, 17, 28]
+
+    print(solution(n, arr1, arr2))
