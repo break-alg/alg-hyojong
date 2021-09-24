38b7f034ee9677d9392425517e61ce2e10f98f52
hjmoons <gywhd408@naver.com>
2021-09-24 23:43:16 +0900

    fix file location and add kakao's problem #3

---
+++ kakao/problem-03.py
@@ -0,0 +1,29 @@
+# 캐시
+# 도시 이름을 입력받아 DB에서 결과를 가져오는데 성능을 개선하는 문제
+# [파이썬 알고리즘 인터뷰 | p.688] 참조
+import collections
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = collections.deque(maxlen=cacheSize)
+
+    for city in cities:
+        city = city.lower()
+        # 캐시 히트 시 재삽입 정리
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:   # 캐시 미스 시 삽입만
+            cache.append(city)
+            answer += 5
+
+    return answer
+
+
+if __name__ == '__main__':
+    cacheSize = 3
+    cities = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
+
+    print(solution(cacheSize, cities))
